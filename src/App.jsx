a3f77a34dd12bced91ecc1746e80a51ba2404f68
rohitchamoli <ronykumar4@gmail.com>
2026-01-10 09:29:01 +0530

    what is component and their rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Intro_react from "./0Introduction/Intro_react";
+import Component from "./1Component/Component";
 
 const App = () => {
   return (
     <>
     <Intro_react/>
+    <Component/>
     </>
   );
 };
